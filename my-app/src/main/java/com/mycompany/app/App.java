079d08112edbfa23ff90f9f688b738bf514e344c
Jesse <jesse.xu.20@gmail.com>
2015-02-07 00:07:54 -0500

    check slave

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
 	System.out.println("commit ");
+	System.out.println("new");
         System.out.println( "Hello World!" );
     }
 }
